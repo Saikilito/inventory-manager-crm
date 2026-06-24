2e491f5c0909a6851651bd31ee09ebfc25960edf
Kembert Nieves <kembert.nievesbriceno@externos-cl.cencosud.com>
2026-06-23 22:10:18 -0400

    build(setup): tailwind v4, oklch variables and vite plugin

---
+++ client/vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwind from '@tailwindcss/vite';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react(), tailwind()],
+  resolve: {
+    alias: {
+      '@shared-domain': path.resolve(__dirname, '../shared-domain/src'),
+      '@modules': path.resolve(__dirname, './src/modules'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@contexts': path.resolve(__dirname, './src/contexts'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+    },
+  },
+  server: {
+    port: 3000,
+    host: true,
+  },
+  build: {
+    outDir: 'build',
+  },
+});
